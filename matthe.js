f1a80091b3c7269cbb544528c155a78bf5ae30ef
Matthejs <90551725+Matthejs@users.noreply.github.com>
2021-11-07 12:41:46 +0300

    Add files via upload

---
+++ matthe.js
@@ -0,0 +1,25 @@
+const { Client } = require("discord.js");
+const { channels, staffRoles, unregisterRoles, welcomeSound, staffSound } = require("./settings.json");
+const tokens = process.env.TOKENS.split(" ");
+
+//TOKEN 1
+ tokens.forEach((token, i) => {
+  const client = new Client();
+  let connection;
+  client.on("ready", async () => connection = await client.channels.cache.get(channels[i]).join());
+    
+  client.on("voiceStateUpdate", async (oldState, newState) => {
+    if ((oldState.channelID && !newState.channelID) || (oldState.channelID && newState.channelID && oldState.channelID === newState.channelID) || newState.member.user.bot || newState.channelID !== channels[i]) return;
+    const hasStaff = newState.channel.members.some((x)=> staffRoles.some((r) => x.roles.cache.has(r)));
+    const staffSize = newState.channel.members.filter((x) => staffRoles.some((r) => x.roles.cache.has(r))).size;
+    const unregisterSize = newState.channel.members.filter((x) => unregisterRoles.some((r) => x.roles.cache.has(r))).size;
+    if (!hasStaff && unregisterSize === 1) await connection.play(welcomeSound);
+    else if (hasStaff && staffSize === 1 && unregisterSize === 1) await connection.play(staffSound);
+  });
+
+  
+  client.on("ready", async () => {
+  client.user.setPresence({ activity: { name: "YOUTUBE MATTHE" }, status: "idle" });
+  })
+  client.login(token).then(() => console.log(`${client.user.tag} Aktif!`)).catch(() => console.error(`${token} Tokeni aktif edilemedi!`));
+});
